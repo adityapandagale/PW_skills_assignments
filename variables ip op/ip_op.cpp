aba72815e08c54fccbb23d92a440fcb34206bf89
adityapandagale <145278049+adityapandagale@users.noreply.github.com>
2023-10-16 00:24:36 +0530

    added my first github assignments solutions

---
+++ variables ip op/ip_op.cpp
@@ -0,0 +1,15 @@
+// Q1 - Take 2 integer values in two variables x and y and print their product. (Easy)
+// Sample Input : x=2, y=4 (Both integers)
+// Sample Output : 8
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+
+    int product =a*b;
+    cout<<product<<endl;
+
+}
