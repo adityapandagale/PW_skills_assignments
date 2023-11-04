48cf69a936b284bd2be9dfef36af1b9fa5fb65aa
adityapandagale <145278049+adityapandagale@users.noreply.github.com>
2023-11-04 19:30:11 +0530

    Add files via upload

---
+++ variables ip op/rough.cpp
@@ -0,0 +1,11 @@
+//Q4 - Write a C++ program to calculate the cube of a number.
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    
+    int cube = a*a*a;
+    cout<<cube<<endl;
+}
